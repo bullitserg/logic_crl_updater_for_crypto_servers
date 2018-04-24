5c1eab024999a98944751c8caae134cc8d5f41a0
belims <bullit88@mail.ru>
2018-04-24 15:14:31 +0300

    Тестовая версия без установки и аргументов

---
+++ config.dist.py
@@ -0,0 +1,10 @@
+timezone = +3           # часовой пояс (от UTC)
+timeout = 100000         # время после истечения next_update, в течение которого необходимо пытаться обновить, seconds
+in_future_time = 600    # время до истечения next_update, в течение которого необходимо пытаться обновить, seconds
+sleep_time = 10         # время задержки выполнения, seconds
+max_install_tries = 2   # максимальное количество попыток установки одного и того же crl
+
+temp_dir = 'C:/Users/belim/PycharmProjects/Crl updater for crypto servers/temp'
+
+log_file = 'log.log'
+error_log_file = 'error.log'
---
+++ queries.py
@@ -0,0 +1,6 @@
+get_urls_query = '''SELECT
+  DISTINCT ci.url
+FROM crl_info ci
+WHERE ci.archive = 0
+AND ci.subjKeyID = %s
+;'''
