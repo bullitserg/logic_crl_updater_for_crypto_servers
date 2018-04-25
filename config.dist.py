69dffa2602fb6c1086f19aa64b90ba4d0e6f340c
belims <bullit88@mail.ru>
2018-04-25 13:11:01 +0300

    Обновление config

--- config.dist.py
+++ config.dist.py
@@ -1,10 +1,13 @@
 timezone = +3           # часовой пояс (от UTC)
-timeout = 100000         # время после истечения next_update, в течение которого необходимо пытаться обновить, seconds
+timeout = 3600          # время после истечения next_update, в течение которого необходимо пытаться обновить, seconds
 in_future_time = 600    # время до истечения next_update, в течение которого необходимо пытаться обновить, seconds
 sleep_time = 10         # время задержки выполнения, seconds
 max_install_tries = 2   # максимальное количество попыток установки одного и того же crl
+server_list = [1, 2, 4, 5]
 
 temp_dir = 'C:/Users/belim/PycharmProjects/Crl updater for crypto servers/temp'
+remote_dir = '/home/application/crl_updater/'
 
-log_file = 'log.log'
-error_log_file = 'error.log'
+log_dir = 'C:/Users/belim/PycharmProjects/Crl updater for crypto servers/'
+log_name_mask = 'crl_updater_server_%s.log'
+
